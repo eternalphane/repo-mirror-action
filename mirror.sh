57ecc25de2019fa0a2ff88fd3ca81a1fc5449985
Zuo Zongyuan <eternalphane@gmail.com>
2023-10-12 14:17:19 +0800

    fix: typos

--- mirror.sh
+++ mirror.sh
@@ -12,13 +12,14 @@ for url in "$SRC_URL" "$DST_URL"; do
 done
 
 # add SSH private keys
-for key in SRC_KEY DST_KEY; do
-    ssh-add "$HOME/.ssh/${!key:-SSH_PRIVATE_KEY}"
-done
+mapfile -t paths < <(for key in SRC_KEY DST_KEY; do echo "$HOME/.ssh/${!key:-SSH_PRIVATE_KEY}"; done | sort -u)
+ssh-add "${paths[@]}"
+trap "ssh-add -d ${paths[*]@Q}" EXIT
 
 # restore cached git repo
 key="mirror-$NAME"
-cache="/var/cache/mirror/$(sha1sum <<< "$GITHUB_WORKFLOW_REF" | awk NF=1)/$NAME"
+cache="$HOME/.cache/mirror/$(sha1sum <<< "$GITHUB_WORKFLOW_REF" | awk NF=1)/$NAME"
+mkdir -p "$cache"
 if "$cwd/cache.mjs" -k "$key" restore "$cache"; then
     # cache hit: update
     git -C "$cache" fetch -p origin
