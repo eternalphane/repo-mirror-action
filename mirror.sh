f9125a61df18ba29b61eabb52635646aa05fef5b
Zuo Zongyuan <eternalphane@gmail.com>
2023-10-12 15:02:28 +0800

    feat: one ssh agent per proc; add cleanup step

--- mirror.sh
+++ mirror.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -eu
 set -o pipefail
-cwd="$(dirname "$(realpath -s "${BASH_SOURCE[-1]}")")"
+cwd=$(dirname "$(realpath -s "${BASH_SOURCE[-1]}")")
+workflow_hash=$(sha1sum <<< "$GITHUB_WORKFLOW_REF" | awk NF=1)
 
-# read config
-config="$1"
+# cleanup
+_cleanup() {
+    set +eu
+    ssh-add -d "${ssh_priv_keys[@]}"
+    rm -f "$SSH_AUTH_SOCK"
+    rm -f "${ssh_priv_keys[@]}"
+}
+trap _cleanup EXIT
+
+# read config (NAME, SRC_URL, DST_URL, SRC_KEY, DST_KEY)
+config=$1
 . <(jq -r 'paths(scalars) as $path | "\($path | join("_") | ascii_upcase)=\(getpath($path) | @sh)"' <<< "$config")
 
 # add SSH known hosts
@@ -11,14 +21,17 @@ for url in "$SRC_URL" "$DST_URL"; do
     ssh-keyscan "$(grep -oP '\w+@\K.+?(?=:)|https?:\/\/\K.+?(?=\/)' <<< "$url")" >> ~/.ssh/known_hosts
 done
 
+# setup SSH agent
+eval "$(ssh-agent -s)"
+
 # add SSH private keys
-mapfile -t paths < <(for key in SRC_KEY DST_KEY; do echo "$HOME/.ssh/${!key:-SSH_PRIVATE_KEY}"; done | sort -u)
-ssh-add "${paths[@]}"
-trap "ssh-add -d ${paths[*]@Q}" EXIT
+mapfile -t ssh_priv_keys < <(for key in SRC_KEY DST_KEY; do echo "$HOME/.ssh/${!key:-SSH_PRIVATE_KEY}"; done | sort -u)
+ssh-add "${ssh_priv_keys[@]}"
+trap "ssh-add -d ${ssh_priv_keys[*]@Q}" EXIT
 
 # restore cached git repo
 key="mirror-$NAME"
-cache="$HOME/.cache/mirror/$(sha1sum <<< "$GITHUB_WORKFLOW_REF" | awk NF=1)/$NAME"
+cache="$HOME/.cache/mirror/$workflow_hash/$NAME"
 mkdir -p "$cache"
 if "$cwd/cache.mjs" -k "$key" restore "$cache"; then
     # cache hit: update
